27ea85b1b56f2576e3490c97b5e1a3bbab2c891a
sonny <sonnyregia96@gmail.com>
2019-08-22 23:38:49 +0700

    sql 22 agustus 2019

--- db_inventory(1).sql
+++ db_inventory(1).sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.9
+-- version 4.8.4
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 09 Agu 2019 pada 05.43
--- Versi server: 10.1.31-MariaDB
--- Versi PHP: 7.2.3
+-- Generation Time: Aug 22, 2019 at 06:37 PM
+-- Server version: 10.1.37-MariaDB
+-- PHP Version: 7.3.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -25,7 +25,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `barang`
+-- Table structure for table `barang`
 --
 
 CREATE TABLE `barang` (
@@ -38,7 +38,7 @@ CREATE TABLE `barang` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `barang`
+-- Dumping data for table `barang`
 --
 
 INSERT INTO `barang` (`id_barang`, `kode_barang`, `nama_barang`, `stok`, `satuan_barang`, `merk_barang`) VALUES
@@ -48,7 +48,7 @@ INSERT INTO `barang` (`id_barang`, `kode_barang`, `nama_barang`, `stok`, `satuan
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `barang_aset`
+-- Table structure for table `barang_aset`
 --
 
 CREATE TABLE `barang_aset` (
@@ -58,70 +58,67 @@ CREATE TABLE `barang_aset` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `barang_aset`
+-- Dumping data for table `barang_aset`
 --
 
 INSERT INTO `barang_aset` (`id_aset`, `kode_aset`, `nama_aset`) VALUES
-(2, '3.02.01.01.003', 'Station Wagon'),
-(3, '3.02.01.04.001', 'Sepeda Motor');
+(4, '3.02.01.04.001', 'Sepeda Motor'),
+(5, '3.02.01.01.003', 'Station Wagon');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `barang_aset_kembali`
+-- Table structure for table `barang_aset_kembali`
 --
 
 CREATE TABLE `barang_aset_kembali` (
   `id_aset_kembali` int(11) NOT NULL,
   `kode_kembali` varchar(20) NOT NULL,
-  `tanggal_balik` varchar(20) NOT NULL,
-  `kode_pinjam` varchar(20) NOT NULL,
-  `kode_aset` varchar(20) NOT NULL,
-  `seri` varchar(10) NOT NULL,
-  `nama_pegawai` varchar(20) NOT NULL,
-  `jabatan` varchar(10) NOT NULL,
-  `keterangan` text NOT NULL,
+  `tanggal_balik` date NOT NULL,
+  `id_aset_pinjam` int(11) NOT NULL,
+  `id_aset` int(11) NOT NULL,
+  `id_aset_sub` int(11) NOT NULL,
+  `terlambat` text NOT NULL,
   `kartu_b` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `barang_aset_kembali`
+-- Dumping data for table `barang_aset_kembali`
 --
 
-INSERT INTO `barang_aset_kembali` (`id_aset_kembali`, `kode_kembali`, `tanggal_balik`, `kode_pinjam`, `kode_aset`, `seri`, `nama_pegawai`, `jabatan`, `keterangan`, `kartu_b`) VALUES
-(1, 'KMB0001', '25 July 2019', 'PJM001', '3.02.01.04.001', '001', 'Dany', 'Staf Keum', '-', '');
+INSERT INTO `barang_aset_kembali` (`id_aset_kembali`, `kode_kembali`, `tanggal_balik`, `id_aset_pinjam`, `id_aset`, `id_aset_sub`, `terlambat`, `kartu_b`) VALUES
+(1, '', '0000-00-00', 10, 4, 6, '18129.916666667', '');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `barang_aset_pinjam`
+-- Table structure for table `barang_aset_pinjam`
 --
 
 CREATE TABLE `barang_aset_pinjam` (
   `kode_pinjam` varchar(10) NOT NULL,
   `id_aset_pinjam` int(11) NOT NULL,
-  `kode_aset` varchar(255) NOT NULL,
-  `seri` varchar(20) NOT NULL,
+  `id_aset` int(11) NOT NULL,
+  `id_aset_sub` int(11) NOT NULL,
   `nama_pegawai` varchar(50) NOT NULL,
   `keterangan` text NOT NULL,
   `jabatan` varchar(50) NOT NULL,
-  `tanggal_pinjam` varchar(20) NOT NULL,
-  `kartu_p` varchar(20) NOT NULL
+  `tanggal_pinjam` date NOT NULL,
+  `kartu_p` varchar(20) NOT NULL,
+  `seri` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `barang_aset_pinjam`
+-- Dumping data for table `barang_aset_pinjam`
 --
 
-INSERT INTO `barang_aset_pinjam` (`kode_pinjam`, `id_aset_pinjam`, `kode_aset`, `seri`, `nama_pegawai`, `keterangan`, `jabatan`, `tanggal_pinjam`, `kartu_p`) VALUES
-('PJM001', 6, '3.02.01.01.003', '001', 'dany', '-', 'staf keum', '26 July 2019', '1'),
-('PJM007', 7, '3.02.01.04.001', '001', 'sony', '-', 'staf keum', '26 July 2019', '2'),
-('PJM008', 8, '3.02.01.04.001', '002', 'zae', 'basemen', 'staf keum', '26 July 2019', '3');
+INSERT INTO `barang_aset_pinjam` (`kode_pinjam`, `id_aset_pinjam`, `id_aset`, `id_aset_sub`, `nama_pegawai`, `keterangan`, `jabatan`, `tanggal_pinjam`, `kartu_p`, `seri`) VALUES
+('', 10, 4, 6, 'caca', '-', 'Staf Keum', '2019-08-22', '1', '');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `barang_aset_pinjam_detail`
+-- Table structure for table `barang_aset_pinjam_detail`
 --
 
 CREATE TABLE `barang_aset_pinjam_detail` (
@@ -136,7 +133,7 @@ CREATE TABLE `barang_aset_pinjam_detail` (
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `barang_aset_sub`
+-- Table structure for table `barang_aset_sub`
 --
 
 CREATE TABLE `barang_aset_sub` (
@@ -144,28 +141,28 @@ CREATE TABLE `barang_aset_sub` (
   `tanggal_input` varchar(20) NOT NULL,
   `tahun` varchar(10) NOT NULL,
   `seri` varchar(10) NOT NULL,
-  `nama_aset` varchar(20) NOT NULL,
+  `id_aset` int(11) NOT NULL,
   `detail_aset` varchar(50) NOT NULL,
-  `merk_aset` varchar(20) NOT NULL,
+  `id_merk_aset` int(11) NOT NULL,
   `penguasaan` text NOT NULL,
   `keterangan` text NOT NULL,
   `gambar` varchar(255) NOT NULL,
-  `satuan_aset` varchar(20) NOT NULL
+  `id_satuan_aset` int(11) NOT NULL,
+  `grup` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `barang_aset_sub`
+-- Dumping data for table `barang_aset_sub`
 --
 
-INSERT INTO `barang_aset_sub` (`id_aset_sub`, `tanggal_input`, `tahun`, `seri`, `nama_aset`, `detail_aset`, `merk_aset`, `penguasaan`, `keterangan`, `gambar`, `satuan_aset`) VALUES
-(3, '25 July 2019', '2015', '001', 'Sepeda Motor', '', 'Suzuki', 'milik sendiri', '-', '', 'Unit'),
-(4, '25 July 2019', '2015', '001', 'Station Wagon', '', 'Honda', 'milik sendiri', '-', '', 'Unit'),
-(5, '25 July 2019', '2016', '002', 'Sepeda Motor', '', 'Honda', '', '', '', 'Unit');
+INSERT INTO `barang_aset_sub` (`id_aset_sub`, `tanggal_input`, `tahun`, `seri`, `id_aset`, `detail_aset`, `id_merk_aset`, `penguasaan`, `keterangan`, `gambar`, `id_satuan_aset`, `grup`) VALUES
+(6, '20 August 2019', '2015', '001', 4, '', 3, 'milik sendiri', '-', '', 2, '1'),
+(7, '20 August 2019', '2016', '001', 5, '', 4, 'milik sendiri', '-', '', 2, '1');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `barang_keluar`
+-- Table structure for table `barang_keluar`
 --
 
 CREATE TABLE `barang_keluar` (
@@ -178,7 +175,7 @@ CREATE TABLE `barang_keluar` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Trigger `barang_keluar`
+-- Triggers `barang_keluar`
 --
 DELIMITER $$
 CREATE TRIGGER `keluar` AFTER INSERT ON `barang_keluar` FOR EACH ROW BEGIN
@@ -198,7 +195,7 @@ DELIMITER ;
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `barang_masuk`
+-- Table structure for table `barang_masuk`
 --
 
 CREATE TABLE `barang_masuk` (
@@ -211,14 +208,14 @@ CREATE TABLE `barang_masuk` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `barang_masuk`
+-- Dumping data for table `barang_masuk`
 --
 
 INSERT INTO `barang_masuk` (`id_barang_masuk`, `kode_barang`, `jumlah`, `harga`, `supplier`, `tanggal`) VALUES
 (26, 'BRG0002', 2, 500000, 'asd', '24 July 2019');
 
 --
--- Trigger `barang_masuk`
+-- Triggers `barang_masuk`
 --
 DELIMITER $$
 CREATE TRIGGER `kembali_masuk` AFTER DELETE ON `barang_masuk` FOR EACH ROW BEGIN
@@ -238,7 +235,7 @@ DELIMITER ;
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `detail_kembali`
+-- Table structure for table `detail_kembali`
 --
 
 CREATE TABLE `detail_kembali` (
@@ -249,7 +246,7 @@ CREATE TABLE `detail_kembali` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `detail_kembali`
+-- Dumping data for table `detail_kembali`
 --
 
 INSERT INTO `detail_kembali` (`kode_kembali`, `kode_aset`, `kode_pinjam`, `id_aset_detatil_kembali`) VALUES
@@ -258,7 +255,34 @@ INSERT INTO `detail_kembali` (`kode_kembali`, `kode_aset`, `kode_pinjam`, `id_as
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `merk_aset`
+-- Table structure for table `kodeurut`
+--
+
+CREATE TABLE `kodeurut` (
+  `kodeurut` varchar(10) NOT NULL,
+  `seri` varchar(10) NOT NULL,
+  `nama_aset` varchar(200) NOT NULL,
+  `tanggal_input` varchar(20) NOT NULL,
+  `tahun` varchar(10) NOT NULL,
+  `id_urut` int(11) NOT NULL,
+  `merk_aset` varchar(50) NOT NULL,
+  `penguasaan` text NOT NULL,
+  `keterangan` text NOT NULL,
+  `satuan_aset` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `kodeurut`
+--
+
+INSERT INTO `kodeurut` (`kodeurut`, `seri`, `nama_aset`, `tanggal_input`, `tahun`, `id_urut`, `merk_aset`, `penguasaan`, `keterangan`, `satuan_aset`) VALUES
+('BMN0001', '001', 'Sepeda Motor', '13 August 2019', '2015', 1, 'Suzuki', 'milik sendiri', 'basemen', 'Unit'),
+('BMN0002', '003', 'Sepeda Motor', '13 August 2019', '2015', 2, 'Honda', 'milik sendiri', 'basemen', 'Unit');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `merk_aset`
 --
 
 CREATE TABLE `merk_aset` (
@@ -267,7 +291,7 @@ CREATE TABLE `merk_aset` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `merk_aset`
+-- Dumping data for table `merk_aset`
 --
 
 INSERT INTO `merk_aset` (`id_merk_aset`, `merk_aset`) VALUES
@@ -277,7 +301,7 @@ INSERT INTO `merk_aset` (`id_merk_aset`, `merk_aset`) VALUES
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `merk_barang`
+-- Table structure for table `merk_barang`
 --
 
 CREATE TABLE `merk_barang` (
@@ -286,7 +310,7 @@ CREATE TABLE `merk_barang` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `merk_barang`
+-- Dumping data for table `merk_barang`
 --
 
 INSERT INTO `merk_barang` (`id_merk`, `merk_barang`) VALUES
@@ -296,7 +320,7 @@ INSERT INTO `merk_barang` (`id_merk`, `merk_barang`) VALUES
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `ruang`
+-- Table structure for table `ruang`
 --
 
 CREATE TABLE `ruang` (
@@ -307,7 +331,7 @@ CREATE TABLE `ruang` (
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `ruang_detail`
+-- Table structure for table `ruang_detail`
 --
 
 CREATE TABLE `ruang_detail` (
@@ -322,7 +346,7 @@ CREATE TABLE `ruang_detail` (
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `satuan_aset`
+-- Table structure for table `satuan_aset`
 --
 
 CREATE TABLE `satuan_aset` (
@@ -331,7 +355,7 @@ CREATE TABLE `satuan_aset` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `satuan_aset`
+-- Dumping data for table `satuan_aset`
 --
 
 INSERT INTO `satuan_aset` (`satuan_aset`, `id_satuan_aset`) VALUES
@@ -341,7 +365,7 @@ INSERT INTO `satuan_aset` (`satuan_aset`, `id_satuan_aset`) VALUES
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `satuan_barang`
+-- Table structure for table `satuan_barang`
 --
 
 CREATE TABLE `satuan_barang` (
@@ -350,7 +374,7 @@ CREATE TABLE `satuan_barang` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `satuan_barang`
+-- Dumping data for table `satuan_barang`
 --
 
 INSERT INTO `satuan_barang` (`id_satuan`, `satuan_barang`) VALUES
@@ -360,7 +384,7 @@ INSERT INTO `satuan_barang` (`id_satuan`, `satuan_barang`) VALUES
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `user`
+-- Table structure for table `user`
 --
 
 CREATE TABLE `user` (
@@ -373,7 +397,7 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `user`
+-- Dumping data for table `user`
 --
 
 INSERT INTO `user` (`id_user`, `username`, `password`, `level`, `nama_lengkap`, `profile`) VALUES
@@ -387,197 +411,209 @@ INSERT INTO `user` (`id_user`, `username`, `password`, `level`, `nama_lengkap`, 
 --
 
 --
--- Indeks untuk tabel `barang`
+-- Indexes for table `barang`
 --
 ALTER TABLE `barang`
   ADD PRIMARY KEY (`id_barang`);
 
 --
--- Indeks untuk tabel `barang_aset`
+-- Indexes for table `barang_aset`
 --
 ALTER TABLE `barang_aset`
   ADD PRIMARY KEY (`id_aset`);
 
 --
--- Indeks untuk tabel `barang_aset_kembali`
+-- Indexes for table `barang_aset_kembali`
 --
 ALTER TABLE `barang_aset_kembali`
   ADD PRIMARY KEY (`id_aset_kembali`);
 
 --
--- Indeks untuk tabel `barang_aset_pinjam`
+-- Indexes for table `barang_aset_pinjam`
 --
 ALTER TABLE `barang_aset_pinjam`
   ADD PRIMARY KEY (`id_aset_pinjam`);
 
 --
--- Indeks untuk tabel `barang_aset_pinjam_detail`
+-- Indexes for table `barang_aset_pinjam_detail`
 --
 ALTER TABLE `barang_aset_pinjam_detail`
   ADD PRIMARY KEY (`id_aset_pinjam_detail`);
 
 --
--- Indeks untuk tabel `barang_aset_sub`
+-- Indexes for table `barang_aset_sub`
 --
 ALTER TABLE `barang_aset_sub`
   ADD PRIMARY KEY (`id_aset_sub`);
 
 --
--- Indeks untuk tabel `barang_keluar`
+-- Indexes for table `barang_keluar`
 --
 ALTER TABLE `barang_keluar`
   ADD PRIMARY KEY (`id_barang_keluar`);
 
 --
--- Indeks untuk tabel `barang_masuk`
+-- Indexes for table `barang_masuk`
 --
 ALTER TABLE `barang_masuk`
   ADD PRIMARY KEY (`id_barang_masuk`);
 
 --
--- Indeks untuk tabel `detail_kembali`
+-- Indexes for table `detail_kembali`
 --
 ALTER TABLE `detail_kembali`
   ADD PRIMARY KEY (`id_aset_detatil_kembali`);
 
 --
--- Indeks untuk tabel `merk_aset`
+-- Indexes for table `kodeurut`
+--
+ALTER TABLE `kodeurut`
+  ADD PRIMARY KEY (`id_urut`);
+
+--
+-- Indexes for table `merk_aset`
 --
 ALTER TABLE `merk_aset`
   ADD PRIMARY KEY (`id_merk_aset`);
 
 --
--- Indeks untuk tabel `merk_barang`
+-- Indexes for table `merk_barang`
 --
 ALTER TABLE `merk_barang`
   ADD PRIMARY KEY (`id_merk`);
 
 --
--- Indeks untuk tabel `ruang`
+-- Indexes for table `ruang`
 --
 ALTER TABLE `ruang`
   ADD PRIMARY KEY (`id_ruang`);
 
 --
--- Indeks untuk tabel `ruang_detail`
+-- Indexes for table `ruang_detail`
 --
 ALTER TABLE `ruang_detail`
   ADD PRIMARY KEY (`id_ruang_detail`);
 
 --
--- Indeks untuk tabel `satuan_aset`
+-- Indexes for table `satuan_aset`
 --
 ALTER TABLE `satuan_aset`
   ADD PRIMARY KEY (`id_satuan_aset`);
 
 --
--- Indeks untuk tabel `satuan_barang`
+-- Indexes for table `satuan_barang`
 --
 ALTER TABLE `satuan_barang`
   ADD PRIMARY KEY (`id_satuan`);
 
 --
--- Indeks untuk tabel `user`
+-- Indexes for table `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`id_user`);
 
 --
--- AUTO_INCREMENT untuk tabel yang dibuang
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT untuk tabel `barang`
+-- AUTO_INCREMENT for table `barang`
 --
 ALTER TABLE `barang`
   MODIFY `id_barang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT untuk tabel `barang_aset`
+-- AUTO_INCREMENT for table `barang_aset`
 --
 ALTER TABLE `barang_aset`
-  MODIFY `id_aset` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_aset` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
--- AUTO_INCREMENT untuk tabel `barang_aset_kembali`
+-- AUTO_INCREMENT for table `barang_aset_kembali`
 --
 ALTER TABLE `barang_aset_kembali`
   MODIFY `id_aset_kembali` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT untuk tabel `barang_aset_pinjam`
+-- AUTO_INCREMENT for table `barang_aset_pinjam`
 --
 ALTER TABLE `barang_aset_pinjam`
-  MODIFY `id_aset_pinjam` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id_aset_pinjam` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
--- AUTO_INCREMENT untuk tabel `barang_aset_pinjam_detail`
+-- AUTO_INCREMENT for table `barang_aset_pinjam_detail`
 --
 ALTER TABLE `barang_aset_pinjam_detail`
   MODIFY `id_aset_pinjam_detail` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT untuk tabel `barang_aset_sub`
+-- AUTO_INCREMENT for table `barang_aset_sub`
 --
 ALTER TABLE `barang_aset_sub`
-  MODIFY `id_aset_sub` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id_aset_sub` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
--- AUTO_INCREMENT untuk tabel `barang_keluar`
+-- AUTO_INCREMENT for table `barang_keluar`
 --
 ALTER TABLE `barang_keluar`
   MODIFY `id_barang_keluar` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT untuk tabel `barang_masuk`
+-- AUTO_INCREMENT for table `barang_masuk`
 --
 ALTER TABLE `barang_masuk`
   MODIFY `id_barang_masuk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 
 --
--- AUTO_INCREMENT untuk tabel `detail_kembali`
+-- AUTO_INCREMENT for table `detail_kembali`
 --
 ALTER TABLE `detail_kembali`
   MODIFY `id_aset_detatil_kembali` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT untuk tabel `merk_aset`
+-- AUTO_INCREMENT for table `kodeurut`
+--
+ALTER TABLE `kodeurut`
+  MODIFY `id_urut` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `merk_aset`
 --
 ALTER TABLE `merk_aset`
   MODIFY `id_merk_aset` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- AUTO_INCREMENT untuk tabel `merk_barang`
+-- AUTO_INCREMENT for table `merk_barang`
 --
 ALTER TABLE `merk_barang`
   MODIFY `id_merk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT untuk tabel `ruang`
+-- AUTO_INCREMENT for table `ruang`
 --
 ALTER TABLE `ruang`
   MODIFY `id_ruang` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT untuk tabel `ruang_detail`
+-- AUTO_INCREMENT for table `ruang_detail`
 --
 ALTER TABLE `ruang_detail`
   MODIFY `id_ruang_detail` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT untuk tabel `satuan_aset`
+-- AUTO_INCREMENT for table `satuan_aset`
 --
 ALTER TABLE `satuan_aset`
   MODIFY `id_satuan_aset` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT untuk tabel `satuan_barang`
+-- AUTO_INCREMENT for table `satuan_barang`
 --
 ALTER TABLE `satuan_barang`
   MODIFY `id_satuan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT untuk tabel `user`
+-- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
   MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
